cf05775d2ce19aee44d4f917c7ff09fff7d202fb
Douglas <douglas.s.santos1603@gmail.com>
2026-01-30 19:26:51 -0300

    fix:database improperly configured

--- core/settings.py
+++ core/settings.py
@@ -99,8 +99,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600
+        default=DATABASE_URL,
+        conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
